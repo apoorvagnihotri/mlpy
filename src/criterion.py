4c6770425e93b639e65b9930b3130b7107eb59d5
Apoorv Ubu18 <apoorvagni@hotmail.com>
2019-01-12 16:01:11 +0530

    criterions good

---
+++ src/criterion.py
@@ -0,0 +1,34 @@
+import utils
+
+class Criterion:
+    """You should not be calling this class diretly.
+    Initialize a derived class below.
+    """
+    def __init__(self):
+        pass
+        
+    def info_gain(self, left, right, current_impurity):
+        '''Finds the information gain w.r.t. previous data in parent
+        node.
+        '''
+        weight_left = float(len(left)) 
+        weight_left /= len(left) + len(right)
+        left_impurity = weight_left * self.impurity(left)
+        right_impurity = (1 - weight_left) * self.impurity(right)
+        new_impurity = current_impurity - left_impurity - right_impurity
+        return new_impurity
+
+class Gini(Criterion):
+    def __init__(self):
+        pass
+    
+    def impurity(self, rows):
+        '''Calculates the gini_impurity corresponding to
+        the rows provided.
+        '''
+        counts = utils.label_counts(rows)
+        total = float(len(rows))
+        gini_im = 1
+        for count in counts.values():
+            gini_im -= (count/total)**2
+        return gini_im
