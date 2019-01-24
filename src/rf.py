b1ecc3309fd0425c79784846ae4a94514ab715a8
Apoorv Agnihotri <apoorvagni@hotmail.com>
2019-01-24 16:56:31 +0530

    mode was returning 2 and more

--- src/rf.py
+++ src/rf.py
@@ -20,10 +20,10 @@ class RandomForest:
     object directly, use a derived class.
     """
     def __init__(self,
-                 method, 
-                 max_depth, 
-                 min_points, 
-                 num_of_trees, 
+                 method,
+                 max_depth,
+                 min_points,
+                 num_of_trees,
                  frac,
                  jobs):
         self.method = method
@@ -34,29 +34,28 @@ class RandomForest:
         self.jobs = jobs
         self.trees = None
         self.roots = None
-        
+
     def train(self, rows):
         """Use this function to train the RF
         """
+        self.rows = rows
         # frac set to root of num_of_features / num_of_features by default
         if isinstance(self.frac, type(None)):
-            p = self.rows.size[1] # num of features
+            p = self.rows.shape[1] # num of features
             self.frac = pow(p - 1, 0.5)/p
-        
+
         # instantiate the decision tree to use
-        self.rows = rows
         tupl = [(self.method, self.max_depth, \
                 self.min_points, self.frac)]
-        tupl = tupl * self.num_of_trees   
+        tupl = tupl * self.num_of_trees
 
         # start jobs
         with Pool(processes=self.jobs) as pool:
-            locations = pool.map(self._thread_train, tupl)
-            self.trees = [l[0] for l in locations]
-            self.roots = [l[1] for l in locations]
+            self.trees = pool.map(self._thread_train, tupl)
+            self.roots = [tree.root for tree in self.trees]
         del self.rows
         return self.roots
-    
+
     def _thread_train(self, tupl):
         """Internal function to be called by individual
         threads
@@ -68,19 +67,19 @@ class RandomForest:
         rows = pd.concat((rows_without_label,
                           self.rows.iloc[:, -1]),
                           axis = 1)
-        dt = self.decision_tree_def(method, 
+        dt = self.decision_tree_def(method,
                                     max_depth,
                                     min_points)
         dt.train(rows)
-        return dt, dt.root
-    
+        return dt
+
     def predict(self, rows):
         """Returns the predictions as a Pd Data
         Frame. To see the individual predction
         look at <object>.preds
         """
         self.predict_rows = rows
-        
+
         # start jobs and return prediction of every dt
         with Pool(processes=self.jobs) as pool:
             preds = pool.map(self._thread_predict,
@@ -96,13 +95,14 @@ class RandomForest:
         """
         # reduce to one prediction @TODO
         big = pd.DataFrame(preds[0]).transpose()
-        
+
         for i in range(1, len(preds)):
             p = pd.DataFrame(preds[i]).transpose()
             big = pd.concat((big, p))
         self.preds = big.transpose() # saving for users to look at
-        return self._prediction_function(big).transpose()
-    
+        combined = self._prediction_function(big).transpose()
+        return combined.iloc[:, :1] # return only one if more
+
     def _thread_predict(self, index):
         """Internal function to be called by individual
         threads
@@ -118,15 +118,15 @@ class RandomForestClassifier(RandomForest):
     def __init__(self,
                  method="gini",
                  max_depth=2,
-                 max_points=None, 
-                 num_of_trees = 4, 
+                 max_points=None,
+                 num_of_trees = 4,
                  frac = 0.7,
                  jobs = 2):
         self.decision_tree_def = DecisionTreeClassifier
         super().__init__(method,
                          max_depth,
-                         max_points, 
-                         num_of_trees, 
+                         max_points,
+                         num_of_trees,
                          frac,
                          jobs)
 
@@ -141,17 +141,17 @@ class RandomForestRegression(RandomForest):
     def __init__(self,
                  method="std",
                  max_depth=2,
-                 max_points=None, 
-                 num_of_trees = 4, 
+                 max_points=None,
+                 num_of_trees = 4,
                  frac = 0.7,
                  jobs = 2):
         self.decision_tree_def = DecisionTreeRegression
         super().__init__(method,
                          max_depth,
-                         max_points, 
-                         num_of_trees, 
+                         max_points,
+                         num_of_trees,
                          frac,
                          jobs)
-    
+
     def _prediction_function(self, preds):
         return pd.DataFrame(preds.mean(axis=0))
